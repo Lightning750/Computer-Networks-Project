485b40c568125e680ca79eb71051fe52ebfd7411
Lightning750 <hpost55115@gmail.com>
2018-04-14 14:53:55 -0500

    Client constructor

--- FileTransferProject/src/FileTransfer/FileTransferClient.java
+++ FileTransferProject/src/FileTransfer/FileTransferClient.java
@@ -1,5 +1,43 @@
 package FileTransfer;
 
-public class FileTransferClient {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class FileTransferClient {
+	private Network.Protocol protocol; 
+	private DatagramSocket UDP_Socket;
+	private Socket TCP_Socket;
+	private int port;
+	private InetAddress serverIP;
+	private DataOutputStream writeBuffer;
+	private DataInputStream readBuffer;
+	
+	FileTransferClient(Network.Protocol p, int port) throws IOException {
+		protocol = p;
+		this.port = port;
+		switch(protocol) {
+		case UDP:
+			UDP_Socket = new DatagramSocket(port);
+			break;
+		case TCP:
+		default:
+			TCP_Socket = new Socket();
+			break;		
+		}
+	}
+	
+	public void beginConnection(InetAddress IP) throws IOException {
+		serverIP = IP;
+		switch(protocol) {
+		case UDP:
+			
+		case TCP:
+		default:
+		}
+	}
 }
