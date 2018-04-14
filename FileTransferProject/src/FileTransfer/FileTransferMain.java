88be015b849047915ca4a770c51663afdffd61bc
Lightning750 <hpost55115@gmail.com>
2018-04-14 15:10:35 -0500

    Merge Fixes

--- FileTransferProject/src/FileTransfer/FileTransferMain.java
+++ FileTransferProject/src/FileTransfer/FileTransferMain.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FileTransferMain {
-	
+
 	private static Network.Protocol protocol;
 
 	public static void main(String[] args) throws IOException {
@@ -13,64 +13,70 @@ public class FileTransferMain {
 		while (userPrompt == 0){
 			System.out.println("Which protocol would you like to use.");
 			System.out.print("Enter 1 for UDP or 2 for TCP: ");
-<<<<<<< HEAD
 			userPrompt = input.nextInt();
 			if (userPrompt == 1){
-
 				System.out.println("You have selected UDP" );
 				protocol = Network.Protocol.UDP;
 			}
 			else if (userPrompt == 2){
-
-=======
-
-
-			x = input.nextInt();
-
-			if (x == 1){
-				System.out.println("You have selected UDP" );
-				protocol = Network.Protocol.UDP;
-			}
-			else if (x == 2){
->>>>>>> dbc8504f88b8b807092b88c23d59541e9757f0ba
-				System.out.println("You have selected TCP");
-				protocol = Network.Protocol.TCP;
-			}
-			else{
-				System.out.println("You have not selected a valid response" );
-				userPrompt = 0;
+				if (userPrompt == 1){
+					System.out.println("You have selected UDP" );
+					protocol = Network.Protocol.UDP;
+				}
+				else if (userPrompt == 2){
+					System.out.println("You have selected TCP");
+					protocol = Network.Protocol.TCP;
+				}
+				else{
+					System.out.println("You have not selected a valid response" );
+					userPrompt = 0;
+				}
 			}
 		}
 		userPrompt = 0;
 		while (userPrompt == 0){
 			System.out.println("Would you like to be the client or server?");
 			System.out.print("Enter 1 for Client or 2 for Server: ");
-<<<<<<< HEAD
 			userPrompt = input.nextInt();
 			if (userPrompt == 1){
-
-=======
-
-			y = input.nextInt();
-
-			if (y == 1){
->>>>>>> dbc8504f88b8b807092b88c23d59541e9757f0ba
-				System.out.println("You have selected Client" );
-			}
-<<<<<<< HEAD
-			else if (userPrompt == 2){
-
-=======
-			else if (y == 2){
->>>>>>> dbc8504f88b8b807092b88c23d59541e9757f0ba
-				System.out.println("You have selected Server");
-			}
-			else{
-				System.out.println("You have not selected a valid response" );
-				userPrompt = 0;
+				if (userPrompt == 1){
+					System.out.println("You have selected Client" );
+					client();
+					return;
+				}
+				else if (userPrompt == 2){
+					System.out.println("You have selected Server");
+					server();
+					return;
+				}
+				else{
+					System.out.println("You have not selected a valid response" );
+					userPrompt = 0;
+				}
 			}
 		}
-		
 	}
+	public static void server() throws IOException {
+		FileTransferServer server;
+		switch(protocol) {
+		case UDP:
+			server = new FileTransferServer(protocol, Network.UDP_PORT);
+			break;
+		case TCP:
+		default:
+			server = new FileTransferServer(protocol, Network.TCP_PORT);
+		}
 
+	}
+	public static void client() throws IOException {
+		FileTransferClient client;
+		switch(protocol) {
+		case UDP:
+			client = new FileTransferClient(protocol, Network.UDP_PORT);
+			break;
+		case TCP:
+		default:
+			client = new FileTransferClient(protocol, Network.TCP_PORT);
+		}
+	}
 }
